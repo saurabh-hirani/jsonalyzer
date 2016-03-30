51da350d72d8f3613595fbd03fd1dccdfde68a73
Saurabh Hirani <saurabh.hirani@gmail.com>
2016-03-30 08:07:42 +0530

    updating version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
   name='jsonalyzer',
   license='MIT',
-  version='0.0.3',
+  version='0.0.4',
   url='https://github.com/saurabh-hirani/jsonalyzer',
   description=('Write callbacks for anlayzing json'),
   author='Saurabh Hirani',
